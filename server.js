866089e022416458fd40e2abe67f0f3998aebf42
lord <m_s_r@outlook.com>
2019-06-26 11:24:11 -0300

    mongo -> mysql (servidor mongo caiu)

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const request = require('request')
 const express = require('express');
 const bodyParser = require('body-parser')
 const dbData = require('./config').default
-const mongoClient = require('mongodb').MongoClient
+// const mongoClient = require('mongodb').MongoClient
+const mysql = require('mysql')
+const connection =  mysql.createConnection({
+                        host: dbData.host,
+                        user: dbData.user,
+                        password: dbData.password,
+                        database: dbData.database
+                    })
 
 const app = express();
 
@@ -35,44 +42,59 @@ app.post('/signup', (req, res) => {
     res.render('signup.ejs')
 })
 
-mongoClient.connect(dbData.uri, {useNewUrlParser: true}, (err, client) => {
-    if (err) return console.log(err)
+connection.connect()
 
-    db = client.db(dbData.dbName)
-
-    let login = 'testeeee'
-    let passwd = 'tessste'
-
-    let exists = async () => { return await search(login) }
-
-    exists().then((result) => {
-        if (result.length === 0) {
-            let insere = async () => { await insert(login, passwd) }
-            insere()
-            console.log('inserido')
-        }
-        else {
-            console.log(result)
-        }
-    })
-
-    app.listen(3000, () => {
-        console.log('Rodando em localhost:3000')
-    })
+connection.query('select * from users', (error, results, fields) => {
+    if (error) throw error;
+    console.log('user:', results[0].login + ';', 'password:', results[0].password);
 })
 
-async function search(login){
-    return new Promise((resolve) => {
-        db.collection('data').find({ _id: login }, (err, results) => {
-            results.toArray((err, result) => {
-                resolve(result)
-            })
-        })
-    })
-}
+connection.end()
 
-async function insert(login, passwd){
-    return new Promise((resolve) => {
-        db.collection('data').insertOne({ _id: login, password: passwd })
-    })
-}
+// console.log('iniciando')
+
+// mongoClient.connect(dbData.uri, {useNewUrlParser: true}, (err, client) => {
+//     if (err) return console.log(err)
+
+//     db = client.db(dbData.dbName)
+
+//     let login = 'testeeee'
+//     let passwd = 'tessste'
+
+//     let exists = async () => { return await search(login) }
+
+//     exists().then((result) => {
+//         if (result.length === 0) {
+//             let insere = async () => { await insert(login, passwd) }
+//             insere()
+//             console.log('inserido')
+//         }
+//         else {
+//             console.log(result)
+//         }
+//     })
+
+    
+// })
+
+app.listen(3000, () => {
+    console.log('Rodando em localhost:3000')
+})
+
+// async function search(login){
+//     return new Promise((resolve) => {
+//         db.collection('data').find({ _id: login }, (err, results) => {
+//             results.toArray((err, result) => {
+//                 resolve(result)
+//             })
+//         })
+//     })
+// }
+
+// async function insert(login, passwd){
+//     return new Promise((resolve) => {
+//         db.collection('data').insertOne({ _id: login, password: passwd })
+//     })
+// }
+
+// console.log('terminando')
